0fa25ce85ba49ec6f27f633066526efbdb3e88be
Gabriel da Silva <gabriel.rodrigues106@fatec.sp.gov.br>
2024-09-11 11:13:37 -0300

    Add files via upload

---
+++ FazerNetBeans/build/classes/FazerNoNetbeans/Main.java
@@ -0,0 +1,10 @@
+package FazerNoNetbeans;
+public class Main {
+    public String nome;
+    public String rgm;
+    int idade;
+    public String github;
+    public String linkedin;
+    public String curso;
+    public String turma;
+}
