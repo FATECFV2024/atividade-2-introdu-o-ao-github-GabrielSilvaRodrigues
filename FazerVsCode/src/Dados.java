3336a478afb16721e791b43620696b21269d8455
Gabriel da Silva <gabriel.rodrigues106@fatec.sp.gov.br>
2024-09-11 11:19:02 -0300

    Add files via upload

---
+++ FazerVsCode/src/Dados.java
@@ -0,0 +1,20 @@
+package FazerNoNetbeans;
+public class Dados {
+    public static void main(String[] args) throws Exception {
+        Main gabriel=new Main();
+        gabriel.nome="Gabriel da Silva Rodrigues";
+        gabriel.rgm="2920482411009";
+        gabriel.idade=19;
+        gabriel.github="https://github.com/GabrielSilvaRodrigues";
+        gabriel.linkedin="https://www.linkedin.com/in/gabriel-silva-9b5818300/";
+        gabriel.curso="Análise e Desenvolvimento de Sistemas";
+        gabriel.turma="Segundo Semestre";
+        System.out.println(gabriel.nome);
+        System.out.println(gabriel.rgm);
+        System.out.println(gabriel.idade);
+        System.out.println(gabriel.github);
+        System.out.println(gabriel.linkedin);
+        System.out.println(gabriel.curso);
+        System.out.println(gabriel.turma);
+    }
+}
